ea7deba9e4b77695d338b7ff3d2c3f7af69deb3f
leo-mesbah <57909891+leo-mesbah@users.noreply.github.com>
2022-10-15 14:39:24 +0200

    Final version
    Il aurait fallu augmenter la vitesse + rotation

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    public ShipBoosting _shipBoosting;
     public CustomInputManager cInputManager;
     public AudioSource source;
     [SerializeField]
@@ -14,22 +15,24 @@ public class AudioManager : MonoBehaviour
     {
         cInputManager = this.GetComponent<CustomInputManager>();
         source = this.GetComponent<AudioSource>();
+        _shipBoosting = this.GetComponentInChildren<ShipBoosting>();
     }
 
 
     private void Update()
     {
+
         if (cInputManager == null)
         {
             return;
         }
 
-        if (cInputManager.myInput != null && cInputManager.myInput.Action4.IsPressed)
+        if (cInputManager.myInput != null && cInputManager.myInput.Action2.IsPressed)
         {
-            source.Play();
-            //Debug.Log("zaklzeajleaz");
+            if (_shipBoosting != null && _shipBoosting.isBoosting)
+                source.PlayOneShot(boost);
         }
-        else
+        else if (cInputManager.myInput != null && !cInputManager.myInput.Action2.IsPressed)
         {
             if (source.isPlaying)
             source.Stop();
